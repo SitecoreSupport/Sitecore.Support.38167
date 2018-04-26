eb44a4cc8be6863af168c8e7f3160a891008c9b8
ArtemNizelnyk <arn@sitecore.net>
2018-04-26 16:23:24 +0300

    Init project files

---
+++ src/Sitecore.Support.38167/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.38167")]
+[assembly: AssemblyProduct("Sitecore.Support.38167")]
+[assembly: ComVisible(false)]
